0c3b10a60a9a005cdddc540ce1aefab34f522f7f
Quincy Michael <michaelkirima865@gmail.com>
2024-10-04 13:53:28 +0300

    Task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+const form = document.getElementById('create-task-form');
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  // event lister adding tasks
+  form.addEventListener('submit', function (e) {
+      e.preventDefault(); 
+      
+      const taskInput = document.getElementById('new-task-description').value;
+      const priority = document.getElementById('task-priority').value;
+      // new item list
+      const newTask = document.createElement('li');
+      newTask.textContent = taskInput;
+
+      if (priority === 'high') {
+          newTask.classList.add('high');
+      } else if (priority === 'medium') {
+          newTask.classList.add('medium');
+      } else {
+          newTask.classList.add('low');
+      }
+      //  delete button to each task if it needs to be deleted
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener('click', function () {
+          newTask.remove();
+      });
+      newTask.appendChild(deleteButton);
+      //  adding the task to the task list
+      taskList.appendChild(newTask);
+      //  clear the form list after inputing a task to be done
+      form.reset();
+  });
 });
